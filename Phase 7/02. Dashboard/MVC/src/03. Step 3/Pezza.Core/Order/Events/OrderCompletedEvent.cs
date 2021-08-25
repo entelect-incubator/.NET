d7f356e188a0e8c730d06a3a48392dc97e1b9347
Jordaan van der Merwe <jordaan.vdmerwe@entelect.com>
2021-08-25 17:12:15 +0200

    Refactor the rest of OrderCompletedEvent and remove email sending

--- Phase 7/02. Dashboard/MVC/src/03. Step 3/Pezza.Core/Order/Events/OrderCompletedEvent.cs
+++ Phase 7/02. Dashboard/MVC/src/03. Step 3/Pezza.Core/Order/Events/OrderCompletedEvent.cs
@@ -12,40 +12,33 @@
     public class OrderCompletedEvent : INotification
     {
         public OrderDTO CompletedOrder { get; set; }
+    }
 
-        public class ArrivalNotificationEventHandler : INotificationHandler<OrderCompletedEvent>
+    public class OrderCompletedEventHandler : INotificationHandler<OrderCompletedEvent>
+    {
+        private readonly IMediator mediator;
+
+        public OrderCompletedEventHandler(IMediator mediator) => this.mediator = mediator;
+
+        public async Task Handle(OrderCompletedEvent notification, CancellationToken cancellationToken)
         {
-            private readonly IMediator mediator;
+            var path = AppDomain.CurrentDomain.BaseDirectory + "\\Email\\Templates\\OrderCompleted.html";
+            var html = File.ReadAllText(path);
 
-            public ArrivalNotificationEventHandler(IMediator mediator) => this.mediator = mediator;
+            html = html.Replace("<%% ORDER %%>", notification.CompletedOrder.Id.ToString());
 
-            public async Task Handle(OrderCompletedEvent notification, CancellationToken cancellationToken)
+            var customer = notification.CompletedOrder?.Customer;
+            var notify = await this.mediator.Send(new CreateNotifyCommand
             {
-                var path = AppDomain.CurrentDomain.BaseDirectory + "\\Email\\Templates\\OrderCompleted.html";
-                var html = File.ReadAllText(path);
-
-                html = html.Replace("<%% ORDER %%>", notification.CompletedOrder.Id.ToString());
-                var emailService = new EmailService
+                Data = new NotifyDTO
                 {
-                    Customer = notification.CompletedOrder?.Customer,
-                    HtmlContent = html
-                };
-
-                var send = await emailService.SendEmail();
-
-                var customer = notification.CompletedOrder?.Customer;
-                var notify = await this.mediator.Send(new CreateNotifyCommand
-                {
-                    Data = new NotifyDTO
-                    {
-                        CustomerId = customer.Id,
-                        DateSent = DateTime.Now,
-                        Email = customer.Email,
-                        Sent = send.Succeeded,
-                        Retry = 0
-                    }
-                });
-            }
+                    CustomerId = customer.Id,
+                    DateSent = DateTime.Now,
+                    Email = customer.Email,
+                    Sent = false,
+                    Retry = 0
+                }
+            });
         }
     }
 }
--- Phase 7/03. Website/ASP.NET/src/01. Step 1/Pezza.Core/Order/Events/OrderCompletedEvent.cs
+++ Phase 7/03. Website/ASP.NET/src/01. Step 1/Pezza.Core/Order/Events/OrderCompletedEvent.cs
@@ -6,60 +6,39 @@
     using System.Threading.Tasks;
     using MediatR;
     using Pezza.Common.DTO;
-    using Pezza.Core.Customer.Queries;
     using Pezza.Core.Email;
     using Pezza.Core.Notify.Commands;
 
     public class OrderCompletedEvent : INotification
     {
         public OrderDTO CompletedOrder { get; set; }
+    }
 
-        public class ArrivalNotificationEventHandler : INotificationHandler<OrderCompletedEvent>
+    public class OrderCompletedEventHandler : INotificationHandler<OrderCompletedEvent>
+    {
+        private readonly IMediator mediator;
+
+        public OrderCompletedEventHandler(IMediator mediator) => this.mediator = mediator;
+
+        public async Task Handle(OrderCompletedEvent notification, CancellationToken cancellationToken)
         {
-            private readonly IMediator mediator;
+            var path = AppDomain.CurrentDomain.BaseDirectory + "\\Email\\Templates\\OrderCompleted.html";
+            var html = File.ReadAllText(path);
 
-            public ArrivalNotificationEventHandler(IMediator mediator) => this.mediator = mediator;
+            html = html.Replace("<%% ORDER %%>", notification.CompletedOrder.Id.ToString());
 
-            public async Task Handle(OrderCompletedEvent notification, CancellationToken cancellationToken)
+            var customer = notification.CompletedOrder?.Customer;
+            var notify = await this.mediator.Send(new CreateNotifyCommand
             {
-                try
+                Data = new NotifyDTO
                 {
-                    var path = AppDomain.CurrentDomain.BaseDirectory + "\\Email\\Templates\\OrderCompleted.html";
-                    var html = File.ReadAllText(path);
-
-                    html = html.Replace("<%% ORDER %%>", notification.CompletedOrder.Id.ToString());
-                    var emailService = new EmailService
-                    {
-                        Customer = notification.CompletedOrder?.Customer,
-                        HtmlContent = html
-                    };
-
-                    var send = await emailService.SendEmail();
-                    if (notification.CompletedOrder.CustomerId.HasValue)
-                    {
-                        var customer = await this.mediator.Send(new GetCustomerQuery { Id = notification.CompletedOrder.CustomerId.Value });
-                        if (customer.Succeeded)
-                        {
-                            var notify = await this.mediator.Send(new CreateNotifyCommand
-                            {
-                                Data = new Common.DTO.NotifyDTO
-                                {
-                                    CustomerId = customer.Data.Id,
-                                    DateSent = DateTime.Now,
-                                    Email = customer.Data.Email,
-                                    Sent = send.Succeeded,
-                                    Retry = 0
-                                }
-                            });
-                        }
-                    }
+                    CustomerId = customer.Id,
+                    DateSent = DateTime.Now,
+                    Email = customer.Email,
+                    Sent = false,
+                    Retry = 0
                 }
-                catch (Exception e)
-                {
-                    Common.Logging.Logging.LogException(e);
-                    throw;
-                }
-            }
+            });
         }
     }
 }
